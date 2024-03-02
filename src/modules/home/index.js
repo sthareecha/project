7f47049ae1e5be09e947eca7b539a39d4595ba19
sthareecha <rs5227516@gmail.com>
2024-03-02 06:45:45 -0800

    first commit

---
+++ src/modules/home/index.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import OverViewComponent from "./OverViewComponent.js";
+import TransactionsComponent from "./TransactionsComponent";
+
+const Container = styled.div`
+  background-color:#C4EF98;
+  color:black;
+  display: flex;
+  font-weight:bold;
+  flex-direction: column;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 360px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HomeComponent = (props) => {
+    const [transactions, updateTransaction] = useState([]);
+    const [expense, updateExpense] = useState(0);
+    const [income, updateIncome] = useState(0);
+
+    const calculateBalance = () => {
+        let exp = 0;
+        let inc = 0;
+        transactions.map((payload) =>
+            payload.type === "EXPENSE"
+                ? (exp = exp + payload.amount)
+                : (inc = inc + payload.amount),
+        );
+        updateExpense(exp);
+        updateIncome(inc);
+    };
+    useEffect(() => calculateBalance(), [transactions]);
+
+    const addTransaction = (payload) => {
+        const transactionArray = [...transactions];
+        transactionArray.push(payload);
+        updateTransaction(transactionArray);
+    };
+    return (
+        <Container>
+            <OverViewComponent
+                expense={expense}
+                income={income}
+                addTransaction={addTransaction}
+            />
+            {transactions?.length ? (
+                <TransactionsComponent transactions={transactions} />
+            ) : (
+                ""
+            )}
+        </Container>
+    );
+};
+export default HomeComponent;
